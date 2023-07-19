55914410c2fddb264cea3dffd4db8b372979834d
Dmitry Shinkaruk <dimashink@gmail.com>
2023-07-19 23:39:39 +0300

    Move to async

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
-use std::net::{Ipv4Addr, UdpSocket};
+use std::net::{Ipv4Addr, SocketAddr};
 
+use async_recursion::async_recursion;
 use cookie_factory::gen_simple;
+use tokio::net::UdpSocket;
 
 mod packet;
 
@@ -12,8 +14,12 @@ use packet::query_type::QueryType;
 use packet::question::DnsQuestion;
 use packet::ResultCode;
 
-fn lookup(qname: &Qname, qtype: QueryType, server: (Ipv4Addr, u16)) -> io::Result<DnsMessage> {
-    let socket = UdpSocket::bind(("0.0.0.0", 0))?;
+async fn lookup(
+    qname: &Qname,
+    qtype: QueryType,
+    server: (Ipv4Addr, u16),
+) -> io::Result<DnsMessage> {
+    let socket = UdpSocket::bind(("0.0.0.0", 0)).await?;
 
     let mut packet = DnsMessage::new();
     packet.header.id = 6666;
@@ -26,10 +32,10 @@ fn lookup(qname: &Qname, qtype: QueryType, server: (Ipv4Addr, u16)) -> io::Resul
 
     let req_buffer = gen_simple(packet.serialize(), Vec::new()).unwrap();
 
-    socket.send_to(&req_buffer, server)?;
+    socket.send_to(&req_buffer, server).await?;
 
     let mut res_buffer = [0u8; MAX_DNS_MSG_SIZE];
-    let (len, _) = socket.recv_from(&mut res_buffer)?;
+    let (len, _) = socket.recv_from(&mut res_buffer).await?;
     let res_buffer = &res_buffer[..len];
 
     let (_, packet) = DnsMessage::parse(res_buffer, &ByteBuffer::new(res_buffer)).unwrap();
@@ -37,31 +43,36 @@ fn lookup(qname: &Qname, qtype: QueryType, server: (Ipv4Addr, u16)) -> io::Resul
 }
 
 const ROOT_SERVERS: [&str; 13] = [
-        "198.41.0.4",
-        "199.9.14.201",
-        "192.33.4.12",
-        "199.7.91.13",
-        "192.203.230.10",
-        "192.5.5.241",
-        "192.112.36.4",
-        "198.97.190.53",
-        "192.36.148.17",
-        "192.58.128.30",
-        "193.0.14.129",
-        "199.7.83.42",
-        "202.12.27.33",
-    ];
+    "198.41.0.4",
+    "199.9.14.201",
+    "192.33.4.12",
+    "199.7.91.13",
+    "192.203.230.10",
+    "192.5.5.241",
+    "192.112.36.4",
+    "198.97.190.53",
+    "192.36.148.17",
+    "192.58.128.30",
+    "193.0.14.129",
+    "199.7.83.42",
+    "202.12.27.33",
+];
 
-fn recursive_lookup(qname: &Qname, qtype: QueryType) -> io::Result<DnsMessage> {
+#[async_recursion]
+async fn recursive_lookup(qname: &Qname, qtype: QueryType) -> io::Result<DnsMessage> {
     use rand::seq::SliceRandom;
-    let mut ns = ROOT_SERVERS.choose(&mut rand::thread_rng()).unwrap().parse::<Ipv4Addr>().unwrap();
+    let mut ns = ROOT_SERVERS
+        .choose(&mut rand::thread_rng())
+        .unwrap()
+        .parse::<Ipv4Addr>()
+        .unwrap();
 
     loop {
         println!("attempting to lookup {qtype:?} entry for {qname} on ns {ns}");
 
         let ns_copy = ns;
         let server = (ns_copy, 53);
-        let response = lookup(qname, qtype, server)?;
+        let response = lookup(qname, qtype, server).await?;
 
         if !response.answers.is_empty() && response.header.flags.rescode == ResultCode::NoError {
             return Ok(response);
@@ -81,7 +92,7 @@ fn recursive_lookup(qname: &Qname, qtype: QueryType) -> io::Result<DnsMessage> {
             None => return Ok(response),
         };
 
-        let recursive_response = recursive_lookup(new_ns_name, QueryType::A)?;
+        let recursive_response = recursive_lookup(new_ns_name, QueryType::A).await?;
 
         if let Some(new_ns) = recursive_response.get_random_a() {
             ns = new_ns;
@@ -91,12 +102,8 @@ fn recursive_lookup(qname: &Qname, qtype: QueryType) -> io::Result<DnsMessage> {
     }
 }
 
-fn handle_query(socket: &UdpSocket) -> io::Result<()> {
-    let mut msg_buf = [0u8; MAX_DNS_MSG_SIZE];
-
-    let (len, src) = socket.recv_from(&mut msg_buf)?;
+async fn handle_query(msg_buf: [u8; MAX_DNS_MSG_SIZE], len: usize) -> io::Result<Vec<u8>> {
     let msg_buf = &msg_buf[..len];
-
     let byte_buffer = ByteBuffer::new(msg_buf);
 
     let (_, mut request) = DnsMessage::parse(msg_buf, &byte_buffer).unwrap();
@@ -110,7 +117,7 @@ fn handle_query(socket: &UdpSocket) -> io::Result<()> {
     if let Some(question) = request.questions.pop() {
         println!("Received query: {:?}", question);
 
-        if let Ok(result) = recursive_lookup(&question.name, question.qtype) {
+        if let Ok(result) = recursive_lookup(&question.name, question.qtype).await {
             packet.questions.push(question);
             packet.header.flags.rescode = result.header.flags.rescode;
 
@@ -136,19 +143,25 @@ fn handle_query(socket: &UdpSocket) -> io::Result<()> {
     packet.update_header();
     let res_buffer = gen_simple(packet.serialize(), Vec::new()).unwrap();
 
-    socket.send_to(&res_buffer, src)?;
-
-    Ok(())
+    Ok(res_buffer)
 }
 
-fn main() -> io::Result<()> {
-    let socket = UdpSocket::bind(("0.0.0.0", 2053))?;
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let local_address = SocketAddr::new("0.0.0.0".parse().unwrap(), 2053);
+    let socket = UdpSocket::bind(("0.0.0.0", 2053)).await?;
 
-    println!("Running DNS server on {}", socket.local_addr()?);
+    println!("Starting DNS server on {local_address}");
 
     loop {
-        if let Err(e) = handle_query(&socket) {
-            eprintln!("An error occurred: {}", e)
-        }
+        let mut msg_buf = [0u8; MAX_DNS_MSG_SIZE];
+
+        let (len, src) = socket.recv_from(&mut msg_buf).await?;
+
+        let worker = tokio::spawn(handle_query(msg_buf, len));
+
+        let (result,) = tokio::join!(worker);
+
+        socket.send_to(&result.expect("failed to join").expect("fail during request handling"), src).await?;
     }
 }
